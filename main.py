1e459697674bce2c1432174815299f004dda3dc5
Dmitriy <dimailyushen@gmail.com>
2024-10-24 14:21:23 +0300

    last

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 import multiprocessing
 from pixelstream import Webrtc_parser
 from mavsdk_test import *
+from image_processor import CVDetect
 import logging
 
 if __name__ == '__main__':
     
-    frame_queue = asyncio.Queue()
+    frame_queue = asyncio.LifoQueue()
 
     async def main():
         mavsdk = DroneControls(connection_string="udp://0.0.0.0:14540", frame_queue=frame_queue, gamepad_mode=True)
@@ -16,11 +17,16 @@ if __name__ == '__main__':
     async def pixel():
         pixell =  Webrtc_parser(frame_queue=frame_queue)
         await pixell.main()
-    
+        
+    async def image_processor():
+        img = CVDetect(frame_queue=frame_queue)
+        await img.detect_screen()
+        
     async def main_loop():
         await asyncio.gather(
             main(),
-            pixel()
+            pixel(),
+            # image_processor()   
         )
 
     asyncio.run(main_loop())
